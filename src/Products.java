a11a7027a052593539f8bb3fac82b0b6620a44ec
Youssef Okeil <youssef_okeil@yahoo.com>
2024-04-28 18:57:50 +0300

    Add check if item new (not complete)

--- src/Products.java
+++ src/Products.java
@@ -4,19 +4,21 @@ public class Products implements Comparable<Integer>{
     private String[] availableColors;
     private String productCode, color, material, printStatement;
     private int gender, numItems;
+    private int size;
     private double price;
     private boolean isAvailable;
     public Products(){
         staticID+=1;
     }
     public Products(String material,String color,int gender,
-                    int numItems, double price){
+                    int numItems, double price, int size){
         this.price=price;
-        this.numItems=numItems;
+        this.setNumItems(numItems);
         this.color=color;
         this.gender=gender;
         this.isAvailable=(numItems>0);
         this.material=material;
+        this.size=size;
     }
 
     public int getNumItems() {
@@ -28,6 +30,14 @@ public class Products implements Comparable<Integer>{
         if(numItems>0){
             this.isAvailable=true;
         }
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
     }
 
     public String getProductCode() {
@@ -55,7 +65,7 @@ public class Products implements Comparable<Integer>{
     }
     public void buy(int numOfItemsInOrder){
         if(isAvailable){
-            if(numOfItemsInOrder<this.numItems){
+            if(numOfItemsInOrder<=this.numItems){
                 this.numItems-=numOfItemsInOrder;
                 this.printStatement="Thank you for buying from us";
             }else{
@@ -63,7 +73,7 @@ public class Products implements Comparable<Integer>{
                         "please specify a number less than %d.",this.numItems);
             }
         }else{
-            this.printStatement="Unfortunately, the item ordered isn't available now";
+            this.printStatement="Unfortunately, the item ordered isn't available right now.";
         }
         System.out.println(this.printStatement);
     }
@@ -96,6 +106,25 @@ public class Products implements Comparable<Integer>{
         } else {
             return -1;
         }
+    }
+    public void displayInfo(){
+        String s="Product is of "+material+"material and "+
+                "of"+color+"color"+
+                "\nNumber of items available: "+numItems+
+                String.format("\nPrice of one item is %.2f EGP ",price);
+        System.out.println(s);
+    }
+    @Override
+    public String toString(){
+        String s=this.material+ "-" + this.color +"-"+ this.size;
+        return s;
+    }
+    @Override
+    public boolean equals(Object otherProduct) {
+        if (this.toString() == otherProduct.toString()) {
+            return true;
+        }
+        return false;
     }
 
 }
---
+++ src/TShirts.java
@@ -0,0 +1,9 @@
+public class TShirts extends Products{
+    public TShirts(){
+        super();
+    }
+    public TShirts(String material,String color,int gender,
+                   int numItems, double price,int size){
+        super(material,color,gender,numItems,price,size);
+    }
+}
