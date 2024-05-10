743f6f8ca4a851de59f95b2f414f28cbbc88fe77
AbdelrahmanSomethingSomething <167958380+AbdelrahmanSomethingSomething@users.noreply.github.com>
2024-05-10 10:44:08 +0300

    Delete src directory

--- src/Graphical.java
+++
@@ -1,8 +0,0 @@
-public interface Graphical {
-    // fconstant for fee of making thing graphical
-    final double ADDED_FEE=100;
-    public abstract void makeGraphical();
-        /* describe how to make product graphical */
-
-
-}
